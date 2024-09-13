d3de6e0c12c63f7a7af4dbf2418ef35b10cd9ca5
Hootan Hemmati <hootanhemmati@outlook.com>
2024-09-14 01:18:48 +0330

    Update to Node10
    Fixes #9

--- SendTelegramNotification/task.js
+++ SendTelegramNotification/task.js
@@ -8,18 +8,17 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const tl = require("vsts-task-lib/task");
+const tl = require("azure-pipelines-task-lib/task");
 const Telegram = require('telegraf/telegram');
 const Telegraf = require('telegraf');
 const tgtools = require('./tgtools');
 
-
-if (tl.getBoolInput('getChatId',false)) {
+if (tl.getBoolInput('getChatId', false)) {
     function run() {
         return __awaiter(this, void 0, void 0, function* () {
             try {
                 console.log("You now can get chat IDs from your chats: \n - Use /chat command to get the ID \n - Then /stop to finish the task \n\n *If something goes wrong - clear chat history*");
-                const bot = new Telegraf(tl.getInput('botToken', true),{
+                const bot = new Telegraf(tl.getInput('botToken', true), {
                     telegram: tgtools.getProxyCfg()
                 });
                 bot.command('/chat', (ctx) => ctx.reply(ctx.chat));
@@ -30,66 +29,66 @@ if (tl.getBoolInput('getChatId',false)) {
                 tl.setResult(tl.TaskResult.Failed, err.message);
             }
         });
-    };
+    }
     run();
 }
 else {
-function run() {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-             var token = tl.getInput('botToken',true);
-             var chats = tl.getDelimitedInput('chats',',',false);
-             var body = "";
-             
-              if (tl.getBoolInput("taskStatus",false)) {             
-                switch (tl.getVariable("Agent.JobStatus")) {
-                 case "Succeeded":
-                     body += "\n<b>✅ Task Succeeded</b>"
-                     break;
-                case "Failed":
-                     body += "\n<b>⛔️ Task Failed</b>"
-                     break;
-                case "SucceededWithIssues":
-                     body += "\n<b>⚠️ Task has some Issues</b>"
-                     break;
-                 default:
-                     break;
-             }
-            }
-            if (tl.getBoolInput('buildNumber', false)) {
-                body += "\n<b>#Build-" + tl.getVariable("Build.BuildNumber")+"</b>";
-            } else if (tl.getBoolInput('releaseName', false)) {
-                body += "\n<b>#" + tl.getVariable("Release.ReleaseName")+"</b>";
-            }
-             if (tl.getInput('message', false) !== null) {
-                 var message = tl.getInput('message', false);
-                 body += "\n" + message;
-             }     
-             if (tl.getBoolInput('buildQueuedBy', false)) {
-                 var buildQueuedBy = tl.getVariable("Build.QueuedBy");
-                 body += "\n<b>Build queued by:</b> " + buildQueuedBy;
-             }
-             if (tl.getBoolInput('releaseRequestedFor', false)) {
-                 var releaseRequestedFor = tl.getVariable("Release.RequestedFor");
-                 body += "\n<b>Release queued by:</b> " + releaseRequestedFor;
-             }
-             if (tl.getBoolInput('teamUrl', false)) {
-                var teamLink = tl.getVariable("System.TeamFoundationCollectionUri");
-                var project = tl.getVariable("System.TeamProject");
-                body += "\n<b>Project URL: </b>" + tgtools.tglinkbuilder(teamLink+project, project);
-             }     
+    function run() {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                var token = tl.getInput('botToken', true);
+                var chats = tl.getDelimitedInput('chats', ',', false);
+                var body = "";
 
+                if (tl.getBoolInput("taskStatus", false)) {
+                    switch (tl.getVariable("Agent.JobStatus")) {
+                        case "Succeeded":
+                            body += "\n<b>✅ Task Succeeded</b>";
+                            break;
+                        case "Failed":
+                            body += "\n<b>⛔️ Task Failed</b>";
+                            break;
+                        case "SucceededWithIssues":
+                            body += "\n<b>⚠️ Task has some Issues</b>";
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                if (tl.getBoolInput('buildNumber', false)) {
+                    body += "\n<b>#Build-" + tl.getVariable("Build.BuildNumber") + "</b>";
+                }
+                else if (tl.getBoolInput('releaseName', false)) {
+                    body += "\n<b>#" + tl.getVariable("Release.ReleaseName") + "</b>";
+                }
+                if (tl.getInput('message', false) !== null) {
+                    var message = tl.getInput('message', false);
+                    body += "\n" + message;
+                }
+                if (tl.getBoolInput('buildQueuedBy', false)) {
+                    var buildQueuedBy = tl.getVariable("Build.QueuedBy");
+                    body += "\n<b>Build queued by:</b> " + buildQueuedBy;
+                }
+                if (tl.getBoolInput('releaseRequestedFor', false)) {
+                    var releaseRequestedFor = tl.getVariable("Release.RequestedFor");
+                    body += "\n<b>Release queued by:</b> " + releaseRequestedFor;
+                }
+                if (tl.getBoolInput('teamUrl', false)) {
+                    var teamLink = tl.getVariable("System.TeamFoundationCollectionUri");
+                    var project = tl.getVariable("System.TeamProject");
+                    body += "\n<b>Project URL: </b>" + tgtools.tglinkbuilder(teamLink + project, project);
+                }
 
-             const telegram = new Telegram(token, tgtools.getProxyCfg());
-             chats.forEach(chat => {
-                 telegram.sendMessage(chat,body, {parse_mode: 'HTML'});   
-             });         
-             console.log('Message sent!');
-        }
-        catch (err) {
-            tl.setResult(tl.TaskResult.Failed, err.message);
-        }
-    });
-}
-run();
+                const telegram = new Telegram(token, tgtools.getProxyCfg());
+                chats.forEach(chat => {
+                    telegram.sendMessage(chat, body, { parse_mode: 'HTML' });
+                });
+                console.log('Message sent!');
+            }
+            catch (err) {
+                tl.setResult(tl.TaskResult.Failed, err.message);
+            }
+        });
+    }
+    run();
 }
